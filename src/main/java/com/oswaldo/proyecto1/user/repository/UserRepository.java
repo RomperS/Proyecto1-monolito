52f0118d97138a785216860596906c2a9a607150
RomperS <Oswaldolo663@gmail.com>
2025-06-29 01:34:22 -0500

    refactor: Change return type of findByName to Optional in UserRepository

--- src/main/java/com/oswaldo/proyecto1/user/repository/UserRepository.java
+++ src/main/java/com/oswaldo/proyecto1/user/repository/UserRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
     List<UserModel> findByUsernameContainingIgnoreCase(String username);
-    List<UserModel> findByName(String name);
+    Optional<UserModel> findByName(String name);
     Boolean existsByUsername(String username);
     Boolean existsByUsernameAndIdIsNot(String username, Integer id);
 }
