0abb8267d017f57465bb83ddbbb3e5548aa4ee60
RomperS <Oswaldolo663@gmail.com>
2025-06-29 00:32:24 -0500

    feat: Add method to find OrderDetails by orderId, productId, and quantity

--- src/main/java/com/oswaldo/proyecto1/order/repository/OrderDetailsRepository.java
+++ src/main/java/com/oswaldo/proyecto1/order/repository/OrderDetailsRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderDetailsRepository extends JpaRepository<OrderDetailsModel, Integer> {
     List<OrderDetailsModel> findByOrderId(Integer orderId);
     List<OrderDetailsModel> findByProductId(Integer productId);
+    Optional<OrderDetailsModel> findByOrderIdAndProductIdAndQuantity(Integer orderId, Integer productId, int quantity);
+
 }
