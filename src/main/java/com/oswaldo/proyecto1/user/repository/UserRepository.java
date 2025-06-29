8e21a690872c1d312a99080a6a38fffb2c7cf91d
RomperS <Oswaldolo663@gmail.com>
2025-06-29 01:56:14 -0500

    feat: Add findByName method to UserRepository

--- src/main/java/com/oswaldo/proyecto1/user/repository/UserRepository.java
+++ src/main/java/com/oswaldo/proyecto1/user/repository/UserRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
     List<UserModel> findByUsernameContainingIgnoreCase(String username);
+    Optional<UserModel> findByUsername(String username);
     List<UserModel> findByName(String name);
     Boolean existsByUsername(String username);
     Boolean existsByUsernameAndIdIsNot(String username, Integer id);
