21e7e03c782588ec572707d7aaeb00dc37d02eba
RomperS <Oswaldolo663@gmail.com>
2025-06-29 06:44:04 -0500

    Feat: Add ErrorMessage.java to util package

---
+++ src/main/java/com/oswaldo/proyecto1/util/ErrorMessages.java
@@ -0,0 +1,9 @@
+package com.oswaldo.proyecto1.util;
+
+public class ErrorMessages {
+
+    public static final String ENTITY_WITH_ID_NOT_FOUND = "%s with id %s not found";
+
+    private ErrorMessages() {
+    }
+}
