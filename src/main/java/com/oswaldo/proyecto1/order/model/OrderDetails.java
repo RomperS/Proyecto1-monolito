e239eb5aea4a102376dcbf1acd44b663ee16c8e0
RomperS <Oswaldolo663@gmail.com>
2025-06-29 03:11:35 -0500

    Feat: Add lazy loading for OrderDetails-Product relationship

--- src/main/java/com/oswaldo/proyecto1/order/model/OrderDetails.java
+++ src/main/java/com/oswaldo/proyecto1/order/model/OrderDetails.java
@@ -23,7 +23,7 @@ public class OrderDetails {
     @JoinColumn(name = "order_id")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "product_id")
     private Product product;
 
